559f0fb805677fdb044fbfba39f74a8fc4ec2d33
chendbox <chendbox@gmail.com>
2023-02-21 15:54:01 -0500

    test_4_functions

--- shopping_cart.py
+++ shopping_cart.py
@@ -3,22 +3,35 @@ from typing import List
 
 class ShoppingCart:
     def __init__(self, max_size: int) -> None:
-        self.items: List[str] = []
+        self.items: List[str] =  []
         self.max_size = max_size
+     
+
 
     def add(self, item: str):
         if self.size() == self.max_size:
-            raise OverflowError("cannot add more items")
+            raise OverflowError("cannot add more item")
         self.items.append(item)
+  
 
     def size(self) -> int:
         return len(self.items)
 
     def get_items(self) -> List[str]:
-        return self.items
+       return self.items
+   
 
     def get_total_price(self, price_map):
+        # cart = ShoppingCart(5)
+        # cart.add("apple")
+        # cart.add("orange")
+
+        # price_map = {"apple": 1.0, "orange": 2.0}
+        # assert cart.get_total_price(price_map) == 3.0
+        # pass
         total_price = 0
         for item in self.items:
-            total_price += price_map.get(item)
-        return total_price
+            total_price = 0
+            for item in self.items:
+                total_price += price_map.get(item)
+            return total_price
--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,24 +1,21 @@
-from unittest.mock import Mock
-from item_database import ItemDatabase
 from shopping_cart import ShoppingCart
 import pytest
-
+from item_database import ItemDatabase
+from unittest.mock import Mock
 
 @pytest.fixture
 def cart():
-    # All setup for the cart here...
     return ShoppingCart(5)
-
 
 def test_can_add_item_to_cart(cart):
     cart.add("apple")
-    assert cart.size() == 1
 
+    # assert cart.size() == 1
 
 def test_when_item_added_then_cart_contains_item(cart):
+
     cart.add("apple")
     assert "apple" in cart.get_items()
-
 
 def test_when_add_more_than_max_items_should_fail(cart):
     for _ in range(5):
@@ -27,17 +24,21 @@ def test_when_add_more_than_max_items_should_fail(cart):
     with pytest.raises(OverflowError):
         cart.add("apple")
 
-
 def test_can_get_total_price(cart):
-    cart.add("apple")
-    cart.add("orange")
-    item_database = ItemDatabase()
+        cart.add("apple")
+        cart.add("orange")
+        # cart.add("orange")
+        item_database = ItemDatabase
 
-    def mock_get_item(item: str):
-        if item == "apple":
-            return 1.0
-        if item == "orange":
-            return 2.0
+        def mock_get_item(item: str):
+            if item == "apple":
+                return 1.0
+            if item == "orange":
+                return 2.0
 
-    item_database.get = Mock(side_effect=mock_get_item)
-    assert cart.get_total_price(item_database) == 3.0
+        
+        item_database.get = Mock(side_effect = mock_get_item)
+        # price_map = {"apple": 1.0, "orange": 2.0}
+        assert cart.get_total_price(item_database) == 3.0
+
+    
